33ac1e404d0228dd881dc97724484a7fa994bd02
Rush116 <71171187a@gmail.com>
2026-03-02 22:45:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dawnknight-light-hub/",
+  base: mode === "production" ? "/dawnknight-light-hub/" : "/",
   server: {
     host: "::",
     port: 8080,
